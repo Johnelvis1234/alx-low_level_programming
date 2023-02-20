91c345c4794540208e23d1e8ac9161f635668913
johnelvis1234 <elvis_onyenemere@yahoo.com>
2023-02-20 00:21:35 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  main - A c program that print a line to the standard error
+ *  main - A c program that print that piece of art is useful
  *  Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 
 return (1);
 }
