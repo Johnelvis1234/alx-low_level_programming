3397ac6a6641ad13002b28d9ef806fa253b55614
johnelvis1234 <elvis_onyenemere@yahoo.com>
2023-02-20 01:30:08 -0800

    2-print_alphabet.c

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print the alphabet in lowercase\n
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch= 'a'; ch<= 'z';ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return(0);
+}
